e1d4d4b056b0e53c15d9430f2b431a70da57a720
Jean-Alexandre Miguel <jeannot262@gmail.com>
2025-02-13 11:31:35 +0100

    back1

---
+++ models/users.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  firstname: String,
+  username: String,
+  password: String,
+  token: String,
+  //   canBookmark: Boolean,
+});
+
+const User = mongoose.model("users", userSchema);
+
+module.exports = User;
